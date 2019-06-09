5494413ca675fc41a0a6f49169b1c1df95a09bce
Hanokh Aloni <Hanokh.Aloni@ngsoft.com>
2019-06-09 22:56:14 +0300

    Reordered + finished implemeting question 1

--- DailyCode/Javascript/answers/question1.js
+++ DailyCode/Javascript/answers/question1.js
@@ -2,13 +2,24 @@
 // For example, given [10, 15, 3, 7] and k of 17, return true since 10 + 7 is 17.
 // Bonus: Can you do this in one pass?
 
-function areTwoNumbersAddUpToK(numbers, k) {
+function areTwoNumbersAddUpToK_naive(numbers, k) {
   for (i = 0; i < numbers.length; i++) {
     for (j = 0; j < numbers.length; j++) {
       if (numbers[i] + numbers[j] === k) {
         return true;
       }
     }
+  }
+  return false;
+}
+
+function areTwoNumbersAddUpToK(numbers, k) {
+  var numbersSet = new Set();
+  for (i = 0; i < numbers.length; i++) {
+    if (numbersSet.has(k - numbers[i])) {
+      return true;
+    }
+    numbersSet.add(numbers[i]);
   }
   return false;
 }
